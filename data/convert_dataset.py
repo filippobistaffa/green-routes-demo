940a06aa455fe50ffb9f9b95034b930393da5921
Filippo Bistaffa <filippo.bistaffa@gmail.com>
2024-03-14 13:02:42 +0100

    Merge AQI datasets in one CSV

--- data/convert_dataset.py
+++ data/convert_dataset.py
@@ -7,17 +7,23 @@ import pyproj
 if __name__ == "__main__":
 
     parser = ap.ArgumentParser()
-    parser.add_argument('--aqi', type=str, default='2022_tramer_no2_mapa_qualitat_aire_bcn.gpkg')
+    parser.add_argument('--no2', type=str, default='2022_tramer_no2_mapa_qualitat_aire_bcn.gpkg')
+    parser.add_argument('--pm25', type=str, default='2022_tramer_pm2-5_mapa_qualitat_aire_bcn.gpkg')
+    parser.add_argument('--pm10', type=str, default='2022_tramer_pm10_mapa_qualitat_aire_bcn.gpkg')
     parser.add_argument('--map', type=str, default='BCN_GrafVial_Trams_ETRS89_CSV.csv')
-    parser.add_argument('--output', type=str, default='2022_no2.csv')
+    parser.add_argument('--output', type=str, default='2022.csv')
     args, additional = parser.parse_known_args()
 
     # air quality data (only contains the road IDs, e.g., "T00001B")
-    aqi = gpd.read_file(args.aqi)
+    no2 = gpd.read_file(args.no2)
+    pm25 = gpd.read_file(args.pm25)
+    pm10 = gpd.read_file(args.pm10)
 
     # map road IDs to actual coordinates
-    map = pd.read_csv(args.map, encoding="latin-1", index_col=0, sep=';')
-    df = pd.merge(map[['COORD_X','COORD_Y','C_Tram']], aqi[['TRAM', 'Rang']], left_on='C_Tram', right_on='TRAM').drop(columns=['C_Tram'])
+    df = pd.read_csv(args.map, encoding="latin-1", index_col=0, sep=';')
+    df = pd.merge(df[['COORD_X','COORD_Y','C_Tram']], no2[['TRAM', 'Rang']], left_on='C_Tram', right_on='TRAM').drop(columns=['C_Tram']).rename(columns={'Rang': 'NO2'})
+    df = pd.merge(df, pm25[['TRAM', 'Rang']]).rename(columns={'Rang': 'PM25'})
+    df = pd.merge(df, pm10[['TRAM', 'Rang']]).rename(columns={'Rang': 'PM10'})
 
     # define UTM zone and projection
     utm_zone = 31
